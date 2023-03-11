fe14b1494a73acdca3e11e223fd9a2cf9e75f7e1
in-ch <sxin2949@naver.com>
2023-03-11 12:31:53 +0900

    feat(common-library): add red component

---
+++ packages/common-library/src/Container.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Red = styled.div`
+    width:100px;
+    height:100px;
+    background-color:red;
+`;
+
+export const Container = () => {
+
+    return (
+        <Red />
+    );
+}
